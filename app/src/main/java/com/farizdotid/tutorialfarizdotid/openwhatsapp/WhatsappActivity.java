20155cdd543073b1a8dbead5f0388cbbd85c426b
farizdotid <contact.farizramadhan@gmail.com>
2019-06-04 15:25:45 +0700

    add comment on code whatsappactivity

--- app/src/main/java/com/farizdotid/tutorialfarizdotid/openwhatsapp/WhatsappActivity.java
+++ app/src/main/java/com/farizdotid/tutorialfarizdotid/openwhatsapp/WhatsappActivity.java
@@ -35,20 +35,32 @@ public class WhatsappActivity extends AppCompatActivity {
         btnOpenWhatsapp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*
+                substring(1) ini artinya kita akan menghapus karakter pertama dari sebuah string.
+                Misalnya user meng-inputkan 0858 maka angka pertama dari 0858 yaitu 0 akan dihapus,
+                karena kebutuhan kita titdak perlu memakai angka 0.
+                 */
                 String nomorHp = etNomorHp.getText().toString().substring(1);
-
                 if (nomorHp.isEmpty()) {
                     Toast.makeText(WhatsappActivity.this, getString(R.string.error_message_form_empty),
                             Toast.LENGTH_SHORT).show();
                 } else {
-                    openWhatsApp(nomorHp);
+                    /*
+                    Setelah kebutuhan nomor kita sudah di sortir. Maksudnya yang asalnya user input
+                    08586078xxxx maka jadi 8586078xxxxx dan disini kita tambahkan +62 didepan nomor yang
+                    user sudah input. Karena kebutuhan dari api Whatsapp harus memakai kode negara.
+                     */
+                    openWhatsApp("+62" + nomorHp);
                 }
             }
         });
     }
 
+    /*
+    Fungsi untuk membuka aplikasi whatsapp menggunakan API yang disediakan.
+     */
     private void openWhatsApp(String number) {
-        String url = "https://api.whatsapp.com/send?phone=+62" + number;
+        String url = "https://api.whatsapp.com/send?phone=" + number;
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(url));
         startActivity(i);
